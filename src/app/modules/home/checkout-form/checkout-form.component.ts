5f9fd01eed0f734c33995ff502008f105e8bc3d2
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2023-05-22 11:18:24 +0545

    [ADDED]: added input field hightlight on invalid form data in contact form, added toast message for payment bill downloaded

--- src/app/modules/home/checkout-form/checkout-form.component.ts
+++ src/app/modules/home/checkout-form/checkout-form.component.ts
@@ -4,6 +4,8 @@ import { CartComponent } from '../cart/cart.component';
 import { CartService } from 'src/app/services/cart.service';
 import { CartItem } from 'src/app/interface/cart-item';
 import { Subscription } from 'rxjs';
+import { ToastService } from 'src/app/services/toast.service';
+import { ToastType } from '../../shared/toast/toast.modal';
 
 @Component({
   selector: 'app-checkout-form',
@@ -12,38 +14,41 @@ import { Subscription } from 'rxjs';
 })
 export class CheckoutFormComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, private router: Router, private cartService: CartService) { }
+  constructor(private route: ActivatedRoute, private router: Router, private cartService: CartService, private toastService:ToastService) { }
   prevRoute?: string | null;
   cartItems: CartItem[] = [];
   getItemsSubscription?: Subscription;
   subTotal: number = 0;
 
   ngOnInit(): void {
+    setTimeout(()=>{
+      this.toastService.show("Your order receipt has been downloaded", ToastType.success);
+    }, 5000);
     // console.log(this.route.snapshot.paramMap.get('previousRoute'));
-    this.prevRoute = this.route.snapshot.paramMap.get('previousRoute');
-    if(this.prevRoute === null || this.prevRoute !== '/home/cart'){
-      this.router.navigate(['/home/']);
-    }
-    this.getItemsSubscription = this.cartService.getCartDetails().subscribe({
-      next: (data) => {
-        this.cartItems = data;
-        this.calculateSubTotal();  
-      },
-      error: (err) => {
-        console.error(err);
-      }
-    })
+    // this.prevRoute = this.route.snapshot.paramMap.get('previousRoute');
+    // if(this.prevRoute === null || this.prevRoute !== '/home/cart'){
+    //   this.router.navigate(['/home/']);
+    // }
+    // this.getItemsSubscription = this.cartService.getCartDetails().subscribe({
+    //   next: (data) => {
+    //     this.cartItems = data;
+    //     this.calculateSubTotal();  
+    //   },
+    //   error: (err) => {
+    //     console.error(err);
+    //   }
+    // })
   }
 
-  calculateSubTotal(): void {
-    this.subTotal = 0;
-    this.cartItems.forEach((cart) => {
-      this.subTotal = this.subTotal + cart.total;
-    });
-  }
+  // calculateSubTotal(): void {
+  //   this.subTotal = 0;
+  //   this.cartItems.forEach((cart) => {
+  //     this.subTotal = this.subTotal + cart.total;
+  //   });
+  // }
 
 
-  ngOnDestroy(){
-    this.getItemsSubscription?.unsubscribe();
-  }
+  // ngOnDestroy(){
+  //   this.getItemsSubscription?.unsubscribe();
+  // }
 }
